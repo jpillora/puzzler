50c250ea1fa1b6be42b83a895b267c5451c33812
Jaime Pillora <dev@jpillora.com>
2023-12-03 00:27:27 +1100

    decrease the cache, in case you solve it really fast

--- internal/pzlr/x/net.go
+++ internal/pzlr/x/net.go
@@ -6,7 +6,7 @@ import (
 )
 
 func Online(domain string) bool {
-	d := net.Dialer{Timeout: 1 * time.Second}
+	d := net.Dialer{Timeout: 3 * time.Second}
 	conn, err := d.Dial("tcp", domain+":80")
 	if err == nil {
 		conn.Close()
